ee329ae40c0c6ee026f62989bdb93ae8c0b6cb40
Adithya-GS <gsaditya52@gmail.com>
2023-07-12 23:15:28 +0530

    first commit1

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const BASE_API_URL =
     process.env.NODE_ENV === 'production'
-        ? 'https://wastemanagementb.onrender.com'
+        ? 'https://wastemanagementb.onrender.com/api/users'
         : 'http://localhost:8000/api/users'
 
 axios.defaults.headers.common['authorization'] = localStorage.getItem('token')
